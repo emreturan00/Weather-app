fe0761c4b232ad5b55bb8378829a3cbd0f9025a8
Emre Turan <turanemre2600@gmail.com>
2023-10-19 22:32:16 +0300

    caching implemented on service

--- src/main/java/com/dorileon/weather/WeatherApplication.java
+++ src/main/java/com/dorileon/weather/WeatherApplication.java
@@ -2,8 +2,11 @@ package com.dorileon.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dorileon/weather/service/WeatherService.java
+++ src/main/java/com/dorileon/weather/service/WeatherService.java
@@ -7,8 +7,14 @@ import com.dorileon.weather.model.WeatherEntity;
 import com.dorileon.weather.repository.WeatherRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cglib.core.Local;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +23,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = {"weathers"})
 public class WeatherService {
+
+    Logger logger = LoggerFactory.getLogger(WeatherService.class);
 
     private final WeatherRepository weatherRepository;
     private final RestTemplate restTemplate;
@@ -31,7 +40,11 @@ public class WeatherService {
         this.restTemplate = restTemplate;
     }
 
+
+    @Cacheable(key ="#city")
     public WeatherDto getWeatherByCityName(String city){
+
+        logger.info("requested city: " + city);
 
         Optional<WeatherEntity> weatherEntityOptional = weatherRepository.
                 findFirstByRequestedCityNameOrderByUpdatedTimeDesc(city);
@@ -56,7 +69,12 @@ public class WeatherService {
         }
     }
 
-
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "10000")
+    public void clearCache(){
+        logger.info("cache cleared!!!!");
+    }
 
     private WeatherEntity saveWeatherEntity(String city, WeatherResponse weatherResponse){
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
