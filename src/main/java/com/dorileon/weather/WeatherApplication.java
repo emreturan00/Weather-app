a209f3c2ee15bb3b11168dde75d317203af6e26a
Emre Turan <turanemre2600@gmail.com>
2023-10-21 16:50:17 +0300

    swagger openAPI added

--- src/main/java/com/dorileon/weather/WeatherApplication.java
+++ src/main/java/com/dorileon/weather/WeatherApplication.java
@@ -2,7 +2,6 @@ package com.dorileon.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
---
+++ src/main/java/com/dorileon/weather/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.dorileon.weather.config;
+
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI();
+    }
+}
